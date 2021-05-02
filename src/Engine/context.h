344f5a4e5a8c168ee928a4e6d5f12cc8ca50cc2c
Grouflon <remi.bismuth@gmail.com>
2021-05-02 17:19:37 +0200

    [memory_std] moved std custom allocators to their own header file (2)

--- src/Engine/context.h
+++ src/Engine/context.h
@@ -26,27 +26,4 @@ struct YAELIB_API Context
 extern Context g_context;
 YAELIB_API Context& context();
 
-
-
-// Std allocators for std containers
-template <class T>
-struct DefaultAllocator
-{
-	typedef T value_type;
-	DefaultAllocator() = default;
- 	template <class U> constexpr DefaultAllocator(const DefaultAllocator<U>&) noexcept {}
-	T* allocate(std::size_t _n) noexcept
-	{
-		return (T*)context().defaultAllocator->allocate(_n * sizeof(T));
-	}
-	void deallocate(T* _p, std::size_t) noexcept
-	{
-		context().defaultAllocator->deallocate(_p);
-	}
-};
-template <class T, class U>
-bool operator==(const DefaultAllocator<T>&, const DefaultAllocator<U>&) { return true; }
-template <class T, class U>
-bool operator!=(const DefaultAllocator<T>&, const DefaultAllocator<U>&) { return false; }
-
 } // namespace yae
