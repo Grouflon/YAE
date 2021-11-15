09e4be4919b4618d59253a2ec4f5fe28decff47f
Grouflon <remi.bismuth@gmail.com>
2021-11-16 00:31:11 +0100

    [math] added ONE vector constants

--- src/yae/math_types.cpp
+++ src/yae/math_types.cpp
@@ -3,6 +3,9 @@
 namespace yae {
 
 const Vector2 Vector2::ZERO = Vector2(0.f);
+const Vector2 Vector2::ONE = Vector2(1.f);
+
 const Vector3 Vector3::ZERO = Vector3(0.f);
+const Vector3 Vector3::ONE = Vector3(1.f);
 
 } // namespace yae
--- src/yae/math_types.h
+++ src/yae/math_types.h
@@ -1,27 +1,31 @@
 #pragma once
 
+#include <yae/types.h>
+
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 
 namespace yae {
 
-struct Vector2 : public glm::vec2
+struct YAELIB_API Vector2 : public glm::vec2
 {
 	Vector2() {}
 	Vector2(float _value) { x = _value; y = _value; }
 	Vector2(float _x, float _y) { x = _x; y = _y; }
 
 	static const Vector2 ZERO;
+	static const Vector2 ONE;
 };
 
 
-struct Vector3 : public glm::vec3
+struct YAELIB_API Vector3 : public glm::vec3
 {
 	Vector3() {}
 	Vector3(float _value) { x = _value; y = _value; z = _value; }
 	Vector3(float _x, float _y, float _z) { x = _x; y = _y; z = _z; }
 
 	static const Vector3 ZERO;
+	static const Vector3 ONE;
 };
 
 } // namespace yae
