f7a3e618b24d0642886c02747ac01e6996b70a5a
Grouflon <remi.bismuth@gmail.com>
2022-02-16 00:11:15 +0100

    cleaned old premake files

--- generate_vs2019_project_files.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-tools\premake5.exe --file=premake5.lua vs2019
-
-::IF "%1"=="--no-prompt" goto :done
-:: pause so the user can see the output if they double clicked the configure script
-::pause
-
-:done
