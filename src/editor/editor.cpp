93f9f9845a58a76715d596f965328f6d28f1abe4
Grouflon <remi.bismuth@gmail.com>
2022-10-21 09:25:03 +0200

    [editor] mirror debug window

--- src/editor/editor.cpp
+++ src/editor/editor.cpp
@@ -14,16 +14,71 @@ struct EditorInstance
 {
 	bool showMemoryProfiler = false;
 	bool showFrameRate = false;
+	bool showMirrorWindow = false;
 	bool showDemoWindow = false;
+
+	// mirror window
+	mirror::TypeID selectedTypeID = mirror::UNDEFINED_TYPEID;
 
 	MIRROR_CLASS_NOVIRTUAL(EditorInstance)
 	(
 		MIRROR_MEMBER(showMemoryProfiler)();
 		MIRROR_MEMBER(showFrameRate)();
+		MIRROR_MEMBER(showMirrorWindow)();
 		MIRROR_MEMBER(showDemoWindow)();
 	);
 };
 MIRROR_CLASS_DEFINITION(EditorInstance);
+
+void displayTypeTreeNode(EditorInstance* _editorInstance, mirror::TypeDesc* _type, mirror::TypeDesc* _parent = nullptr)
+{
+	mirror::TypeID id = _type->getTypeID();
+	ImGuiTreeNodeFlags nodeFlags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick | (_editorInstance->selectedTypeID == id ? ImGuiTreeNodeFlags_Selected : 0);
+	mirror::Class* clss = _type->asClass();
+
+	if (clss != nullptr && clss->getParents().size() > 0)
+	{
+		if (_parent == nullptr)
+			return;
+
+		bool parentMatch = false;
+		for (mirror::TypeID parentID : clss->getParents())
+		{
+			if (parentID == _parent->getTypeID())
+			{
+				parentMatch = true;
+				break;
+			}
+		}
+		if (!parentMatch)
+			return;
+	}
+
+	bool hasChildren = clss != nullptr && clss->getChildren().size() > 0;
+	if(!hasChildren)
+	{
+		nodeFlags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen; // ImGuiTreeNodeFlags_Bullet
+	}
+
+	bool isNodeOpen = ImGui::TreeNodeEx((void*)(intptr_t)id, nodeFlags, "%s", _type->getName());
+	if (ImGui::IsItemClicked())
+	{
+		_editorInstance->selectedTypeID = id;
+	}
+    if (hasChildren && isNodeOpen)
+    {
+    	for (mirror::TypeID childId : _type->asClass()->getChildren())
+    	{
+    		mirror::TypeDesc* childType = mirror::GetTypeSet().findTypeByID(childId);
+    		if (childType == nullptr)
+    			continue;
+
+    		displayTypeTreeNode(_editorInstance, childType, _type);
+    	}
+    	
+    	ImGui::TreePop();
+    }
+}
 
 void onModuleLoaded(yae::Program* _program, yae::Module* _module)
 {
@@ -85,6 +140,7 @@ void updateApplication(yae::Application* _application, float _dt)
 
         if (ImGui::BeginMenu("Misc"))
         {
+        	changedSettings = ImGui::MenuItem("Mirror", NULL, &editorInstance->showMirrorWindow) || changedSettings;
         	changedSettings = ImGui::MenuItem("ImGui Demo Window", NULL, &editorInstance->showDemoWindow) || changedSettings;
             ImGui::EndMenu();
         }
@@ -161,6 +217,60 @@ void updateApplication(yae::Application* _application, float _dt)
     	}
     	ImGui::End();
 		changedSettings = changedSettings || (previousOpen != editorInstance->showFrameRate);
+    }
+
+    if (editorInstance->showMirrorWindow)
+    {
+    	bool previousOpen = editorInstance->showMirrorWindow;
+		if (ImGui::Begin("Mirror", &editorInstance->showMirrorWindow))
+    	{
+    		ImVec2 regionSize = ImVec2(ImGui::GetWindowContentRegionWidth(), 0);
+    		ImGui::BeginChild("MirrorChildL", ImVec2(regionSize.x * 0.5f, regionSize.y), false);
+    		for (mirror::TypeDesc* type : mirror::GetTypeSet().getTypes())
+    		{
+				displayTypeTreeNode(editorInstance, type, nullptr);
+    		}
+    		ImGui::EndChild();
+
+    		ImGui::SameLine();
+
+    		ImGui::BeginChild("MirrorChildR", ImVec2(0, regionSize.y), true);
+    		if (editorInstance->selectedTypeID != mirror::UNDEFINED_TYPEID)
+    		{
+    			mirror::TypeDesc* type = mirror::FindTypeByID(editorInstance->selectedTypeID);
+    			if (type != nullptr)
+    			{
+    				ImGui::Text("name: %s", type->getName());
+    				ImGui::Text("type: %s", mirror::TypeToString(type->getType()));
+    				if (mirror::Class* clss = type->asClass())
+    				{
+    					size_t membersCount = clss->getMembersCount();
+    					if (membersCount > 0)
+    					{
+    						ImGui::Text("members:");
+
+    						DataArray<mirror::ClassMember*> members(&scratchAllocator());
+    						members.resize(membersCount);
+							clss->getMembers(members.data(), members.size());
+
+    						for (mirror::ClassMember* member : members)
+    						{
+    							mirror::TypeDesc* memberType = member->getType();
+    							ImGui::BulletText("%s (%s)", member->getName(), memberType ? memberType->getName() : "unknown");
+    						}
+    					}
+    					else
+    					{
+    						ImGui::Text("no members");
+    					}
+    				}
+    			}
+    		}
+    		ImGui::EndChild();
+    	}
+    	ImGui::End();
+
+		changedSettings = changedSettings || (previousOpen != editorInstance->showMirrorWindow);
     }
 
     if (editorInstance->showDemoWindow)
